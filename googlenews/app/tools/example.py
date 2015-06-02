0cfd35113754f6594623bbf3780ee68acd9a884e
Molinié Vincent <vincent.molinie@epita.fr>
2015-06-03 00:41:36 +0200

    Lemmatisation and calculation of frequency done

--- googlenews/app/tools/example.py
+++ googlenews/app/tools/example.py
@@ -1,14 +1,43 @@
 from TreeTagger import treetaggerwrapper
 from TreeTagger.TreeTaggerWord import *
+from math import log
 
-# Construction et configuration du wrapper
-tagger = treetaggerwrapper.TreeTagger(TAGLANG='fr',TAGDIR='TreeTagger',
-  TAGINENC='utf-8',TAGOUTENC='utf-8')
+def lemmatisation(array_rss, k):
+    result = {}
+    doc = {}
+    for rss in array_rss:
+        result, doc = lemmatisation(rss, k, occ, doc)
 
-# Utilisation
-tags = tagger.TagText(u"Ceci est un très court texte à étiqueter.")
-data = formatTTG(tags)
+    max_occ = max(result.values())
+    nb_doc = len(array_rss)
+    for key in result:
+        tf = result[key] / max_occ
+        idf = log(nb_doc / doc[key]) + 1
+        #Faire quelquechose avec !!!
+        #To delete
+        print key + ' ' + str(tf) + ' ' + str(idf)
 
-print data[0].word
-print data[0].postag
-print data[0].lemma
+def lemmatisation(input, k, result, doc):
+    # Construction et configuration du wrapper
+    tagger = treetaggerwrapper.TreeTagger(TAGLANG='fr',TAGDIR='TreeTagger',
+                                          TAGINENC='utf-8',TAGOUTENC='utf-8')
+
+    # Utilisation
+    tags = tagger.TagText(input)
+    data = formatTTG(tags)
+
+    liste = []
+
+    i = 0
+    while i <= len(data) - 1:
+        lemma = data[i].lemma
+        if not result.has_key(lemma):
+            result[lemma] = 0
+            doc[lemma] = 1
+        elif lemma not in liste:
+            doc[lemma] += 1
+            liste += [lemma]
+
+        result[lemma] += 1
+        i += 1
+    return result
