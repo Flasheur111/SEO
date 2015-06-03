657284b722f330f76a79d2fc07416e8722a7df04
Molinié Vincent <vincent.molinie@epita.fr>
2015-06-03 22:05:51 +0200

    [googlenews] Converting TreeTagger Wrapper to python 3

--- googlenews/app/tools/lemmatization.py
+++ googlenews/app/tools/lemmatization.py
@@ -3,42 +3,50 @@ from TreeTagger.TreeTaggerWord import *
 from math import log
 
 def lemmatisation(array_rss, k):
-    result = {}
+    occ = {}
     doc = {}
     for rss in array_rss:
-        result, doc = lemmatisation(rss, k, occ, doc)
+        result, doc = lemmatisation_intern(rss, k, occ, doc)
 
     max_occ = max(result.values())
     nb_doc = len(array_rss)
     for key in result:
         tf = result[key] / max_occ
+        #print "NbDoc: " + str(nb_doc) + " Doc: " + str(doc[key])
         idf = log(nb_doc / doc[key]) + 1
         score = tf * idf
         #Faire quelquechose avec !!!
         #To delete
-        print key + ' ' + str(tf) + ' ' + str(idf)
+        print(key + ' ' + str(tf) + ' ' + str(idf))
 
-def lemmatisation(input, k, result, doc):
+def lemmatisation_intern(rss, k, result, doc):
     # Construction et configuration du wrapper
-    tagger = treetaggerwrapper.TreeTagger(TAGLANG='fr',TAGDIR='TreeTagger',
+    tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR='TreeTagger',
                                           TAGINENC='utf-8',TAGOUTENC='utf-8')
 
     # Utilisation
-    tags = tagger.TagText(input)
+    print('rss is ' + rss)
+    tags = tagger.TagText(rss)
     data = formatTTG(tags)
 
     liste = []
 
     i = 0
     while i <= len(data) - k:
-        lemma = data[i].lemma + data[i + 1].lemma + data[i + 3].lemma
-        if not result.has_key(lemma):
+        lemma = data[i].lemma
+        for j in range(k - 1):
+            lemma += " " + data[i + j + 1]
+        if lemma not in result:
             result[lemma] = 0
             doc[lemma] = 1
+            liste += [lemma]
         elif lemma not in liste:
             doc[lemma] += 1
             liste += [lemma]
 
         result[lemma] += 1
         i += 1
-    return result
+    return result, doc
+
+
+lemmatisation(['What I did for love ?'], 1)
