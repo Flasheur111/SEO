88c0083b8b37f6d51640b241787e4963ff9512fe
Mohamed Jeffal <mh.jeffal@gmail.com>
2015-06-05 20:49:48 +0200

    [ArticleParser] Fixing a spelling mistake

--- googlenews/app/tools/article_parser.py
+++ googlenews/app/tools/article_parser.py
@@ -2,14 +2,13 @@ from newspaper import Article, ArticleException
 
 
 class ArticleParser:
-
-    def get_corpus(self, article_url):
-        article = Article(article_url, language='fr')
-        article.download()
+	def get_corpus(self, article_url):
+		article = Article(article_url, language='fr')
+		article.download()
 		try:
-        	article.parse()
-		except ArticleExcetion:
+			article.parse()
+		except ArticleException:
 			return {}
-        return {'title': article.title, 'authors': article.authors,
-                'publish_date': article.publish_date, 'text': article.text,
-                'image': article.top_image}
+		return {'title': article.title, 'authors': article.authors,
+				'publish_date': article.publish_date, 'text': article.text,
+				'image': article.top_image}
