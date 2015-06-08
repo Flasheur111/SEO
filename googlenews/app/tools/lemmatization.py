69de7adf8fd9a7a0b2a6887198e7a15d3d71eed2
Molinié Vincent <vincent.molinie@epita.fr>
2015-06-08 03:45:45 +0200

    [googlenews] Multi Architecture done

--- googlenews/app/tools/lemmatization.py
+++ googlenews/app/tools/lemmatization.py
@@ -3,6 +3,13 @@ from app.tools.TreeTagger.TreeTaggerWord import *
 from math import log
 from operator import itemgetter
 import stop_words
+import platform
+
+if platform.architecture()[0] == '32bit':
+    treetagger_path = 'app/tools/TreeTagger/32'
+else:
+    treetagger_path = 'app/tools/TreeTagger/64'
+
 
 def lemmatisation_full_article(article_rss, k=1, lang='fr', nb_keyword=3):
     titles = lemmatisation(article_rss.keys(), lang, k)
@@ -44,7 +51,7 @@ def lemmatisation(array_rss, lang, k=1):
 
 def lemmatisation_intern(lang, rss, k, result, doc):
     # Construction et configuration du wrapper
-    tagger = treetaggerwrapper.TreeTagger(TAGLANG=lang,TAGDIR='app/tools/TreeTagger',
+    tagger = treetaggerwrapper.TreeTagger(TAGLANG=lang,TAGDIR=treetagger_path,
                                           TAGINENC='utf-8',TAGOUTENC='utf-8')
 
     # Utilisation
